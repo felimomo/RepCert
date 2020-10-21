c7ed0616969c49a080c0f2789e3f770825a923a0
Felipe Montealegre Mora <felimomouni@gmail.com>
2020-10-21 15:59:27 +0200

    now with 100 data points instead of 40

--- stat_test.py
+++ stat_test.py
@@ -38,7 +38,7 @@ if group_name == 's4':
 
 generators = [t,c]
 
-datapts = 40
+datapts = 100
 min_noiseExp=30
 t_surplus = eval(input("surplus random walk length = ")) #random walks up to length 2(t_min + t_surplus)
 
