d161c083f827bcade4b42490d87de7fb588c04b7
Felipe Montealegre Mora <felimomouni@gmail.com>
2020-11-12 18:17:06 +0100

    found bug

--- Scrpt/script-replab.m
+++ Scrpt/script-replab.m
@@ -25,7 +25,8 @@ rep = rep.complexification
 % Save generator images
 i = 1 %indexing of generators starts with 1
 while i < W.nGenerators+1
-  save -v7 [mat2str(i) ".mat"] rep.image(W.generators{i});
+  im = rep.image(W.generators{i})
+  save -v7 [mat2str(i) ".mat"] im;
   i = i+1;
 endwhile
 % decompose rep:
