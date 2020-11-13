c55808e9bf4c0a608d0c22ab5c925d3483169e08
Felipe Montealegre Mora <felimomouni@gmail.com>
2020-11-13 19:32:21 +0100

    added max samples calculator that is lie-friendly

--- Certificates/BundleCertificate.py
+++ Certificates/BundleCertificate.py
@@ -62,7 +62,7 @@ def subrep_tester(repr,basis,t_surplus,error_p,prnt=False):
         dim = len(basis)
         print("Invariant at precision ",epsilon)
         # print("Minimal rand walk length = ", 2*t_min)
-        # print("Max number of samples required for irreducibility: ", rwalk.number_samples(repr,len(basis),epsilon,error_p,t_max))
+        print("Max number of samples required for irreducibility: ", rwalk.number_samples(repr,len(basis),epsilon,error_p,max(t_range)))
         print("Dimension of irrep being tested = ", dim)
         
     if epsilon==1:
