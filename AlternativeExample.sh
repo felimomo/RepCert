84dbab993c59702c7de35e5a4e827ab09a524e6a
Felipe Montealegre Mora <>
2021-01-26 16:22:23 +0100

    space

--- AlternativeExample.sh
+++ AlternativeExample.sh
@@ -2,6 +2,7 @@
 #
 #
 
+echo " "
 echo "Running alternative example for RepCert. The set of group images have been Haar sampled and"
 echo "symmetrized, so pick setting='promise' when prompted."
 echo " "
