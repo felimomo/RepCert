14a9daf2871ecb55150dd00f95d7ec0b6c8638b4
Felipe Montealegre Mora <>
2021-01-26 15:37:56 +0100

    blah

--- scr-replab.m
+++ scr-replab.m
@@ -72,7 +72,7 @@ Sn = replab.S(4)
 nat = Sn.naturalRep;
 
 % big rep
-bigrep = nat;
+bigrep = kron(nat,nat);
 bigrep = bigrep.complexification;
 bigrep = bigrep.unitarize;
 decomp = bigrep.decomposition;
