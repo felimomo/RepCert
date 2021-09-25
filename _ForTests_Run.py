5d5b0872dc72430774e70c1770c9e84694c34237
Felipe Montealegre Mora <felimomouni@gmail.com>
2021-09-25 11:27:40 +0200

    edited group name

--- _ForTests_Run.py
+++ _ForTests_Run.py
@@ -44,7 +44,7 @@ def restrict_to_subrep(repr,basis,setting='promise'):
 
 setting = input("Setting = ")
 flo=0 # just to have it be global, set it more specifically later
-a,b='4','4'
+a,b='10','10'
 # GroupName=f"S{a}wrS{b}wrS{c}"
 GroupName=f"S{a}wrS{b}"
 
